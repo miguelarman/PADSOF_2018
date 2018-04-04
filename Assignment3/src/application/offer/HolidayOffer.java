130591bd541971bfe922c601a51d701112ad6521
Miguel Arconada WIN <miguelarman@gmail.com>
2018-04-05 00:07:15 +0200

    Javadoc del paquete application.offer

--- Assignment3/src/application/offer/HolidayOffer.java
+++ Assignment3/src/application/offer/HolidayOffer.java
@@ -2,33 +2,66 @@ package application.offer;
 
 import java.time.LocalDate;
 
+/**
+ * Class that stores the data of a holiday offer. It is created from the class Offer
+ * 
+ * @author Miguel Arconada (miguel.arconada@estudiante.uam.es) y Alberto
+ *         Gonzalez (alberto.gonzalezk@estudiante.uam.es)
+ *
+ */
 public class HolidayOffer extends Offer {
-
+	
 	/**
-	 * 
+	 * ID needed for the class to be Serializable
 	 */
 	private static final long serialVersionUID = -4991181617749951314L;
+	
+	/**
+	 * Date when the holiday offer ends
+	 */
 	private LocalDate finishDate;
-
+	
+	
+	/**
+	 * Constructor of the class HolidayOffer. It uses the constructor of the superclass Offer
+	 * 
+	 * @param startingLocalDate Starting date of the offer
+	 * @param price Price of the offer
+	 * @param deposit Deposit to be paid for the offer
+	 * @param description Description of the offer
+	 * @param offeredHouse House in which the offer takes place
+	 * @param finishLocalDate Finishing date of the offer
+	 */
 	public HolidayOffer(LocalDate startingLocalDate, Double price, Double deposit, String description, House offeredHouse, LocalDate finishLocalDate) {
 		super(startingLocalDate, price, deposit, description, offeredHouse);
 		this.finishDate = finishLocalDate;
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
+	/**
+	 * Method that returns the type of the offer
+	 * 
+	 * @returns It always returns OfferType.HOLIDAY
+	 */
 	public OfferType getType() {
 		return OfferType.HOLIDAY;
 	}
 	
-	
+	/**
+	 * Getter for the attribute finishDate
+	 * 
+	 * @return finish date of the offer
+	 */
 	
 	public LocalDate getFinishLocalDate() {
 		return finishDate;
 	}
-
+	
+	/**
+	 * Setter method for the attribute finish date
+	 * @param finishDate new finish date of the offer
+	 */
 	public void setFinishDate(LocalDate finishDate) {
 		this.finishDate = finishDate;
 	}
-
 }
--- Assignment3/src/application/offer/Reservation.java
+++ Assignment3/src/application/offer/Reservation.java
@@ -8,54 +8,102 @@ import application.users.Guest;
 
 import exceptions.*;
 
-
+/**
+ * Class that stores all the data needed to book an offer. It uses the classes
+ * Offer and Guest, as the are needed to perform the actions of this class
+ * 
+ * @author Miguel Arconada (miguel.arconada@estudiante.uam.es) y Alberto
+ *         Gonzalez (alberto.gonzalezk@estudiante.uam.es)
+ *
+ */
 public class Reservation implements Serializable{
 	
 	/**
-	 * 
+	 * ID needed for serialization
 	 */
 	private static final long serialVersionUID = 738403020803075461L;
+	
+	/**
+	 * Date in which the offer was booked
+	 */
 	private LocalDate bookingDate;
+	
+	/**
+	 * Client that booked the offer
+	 */
 	private Guest client;
+	
+	/**
+	 * Offer that was booked
+	 */
 	private Offer bookedOffer;
-
+	
+	
+	/**
+	 * Constructor of the class Reservation
+	 * 
+	 * @param bookingDate Date in which the reservation is created
+	 * @param client Client that is booking the offer
+	 * @param bookedOffer Offer that is being booked
+	 */
 	public Reservation(LocalDate bookingDate, Guest client, Offer bookedOffer) {
 		this.bookingDate = bookingDate;
 		this.client = client;
 		this.bookedOffer = bookedOffer;
 	}
 	
+	/**
+	 * Getter method for the bookingDate attribute
+	 * 
+	 * @return Booking date of the reservation
+	 */
 	public LocalDate getBookingDate() {
 		return bookingDate;
 	}
 	
+	/**
+	 * Getter method for the client attribute
+	 * 
+	 * @return Client of the reservation
+	 */
 	public Guest getClient() {
 		return client;
 	}
 	
+	/**
+	 * Getter method for the bookedOffer attribute
+	 * 
+	 * @return Offer of the reservation
+	 */
 	public Offer getBookedOffer() {
 		return bookedOffer;
 	}
 	
 	
-	
+	/**
+	 * Method that cancels a Reservation
+	 */
 	public void cancelReservation() {
 				
 		Guest user = this.client;
 		
 		user.deleteReservation(this);
 		
-		// esto no se si sirve para algo pero por si acaso lo pongo todo a null
+		// This assigning prevent errors in the future
 		this.bookedOffer = null;
 		this.client = null;
 		this.bookingDate = null;
 	}
 	
 	
+	/**
+	 * Method that pays for the reservation. It call the method payOffer()
+	 * 
+	 * @throws NotTheReserverException When the user trying to pay is not the reserver
+	 */
 	public void payReservation() throws NotTheReserverException {
 
-		// comprobar que el usuario que intenta pagar la reserva es el que esta logueado y es el que la ha reservado
-		
+		// We check if the user trying to pay the reservation is the one that booked it
 		if (!this.client.equals(App.getLoggedUser())) {
 			throw new NotTheReserverException(this, App.getLoggedUser());
 		} else {
@@ -69,6 +117,12 @@ public class Reservation implements Serializable{
 	}
 	
 	@Override
+	/**
+	 * Method that returns all the information stored in an object of the class
+	 * Reservation in a printable and readable format
+	 * 
+	 * @return Information stored in the Reservation in a printable format
+	 */
 	public String toString() {
 		// TODO
 		
