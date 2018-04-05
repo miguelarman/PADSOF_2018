d244d93aa61b46521ccb1ac4e6bf49ab72ec41ec
Miguel Arconada WIN <miguelarman@gmail.com>
2018-04-05 09:28:12 +0200

    sdasdadasd

--- Assignment3/src/application/users/HostI.java
+++ Assignment3/src/application/users/HostI.java
@@ -5,7 +5,28 @@ import java.util.List;
 import application.offer.House;
 import exceptions.HouseAlreadyCreatedException;
 
+/**
+ * Interface that models the functionality required for Hosts in the app
+ * 
+ * @author Miguel Arconada (miguel.arconada@estudiante.uam.es) Alberto Gonzalez
+ *         (alberto.gonzalezk@estudiante.uam.es)
+ *
+ */
 public interface HostI {
+	
+	/**
+	 * Method that returns the houses which the Host has created in the app
+	 * 
+	 * @return List with all the houses the host has created
+	 */
 	public List<House> getHouses();
+
+	/**
+	 * Method that adds a House to the list of created houses of a Host
+	 * 
+	 * @param zipCode Zip code of the house
+	 * @param city City of the house
+	 * @throws HouseAlreadyCreatedException When a house has been created with the same data
+	 */
 	public void addHouse(Integer zipCode, String city) throws HouseAlreadyCreatedException;
 }
