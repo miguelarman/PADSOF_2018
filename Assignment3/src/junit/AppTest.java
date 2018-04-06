98d280662f1f72503fbacbb0aa1339a174df0ace
Miguel Arconada WIN <miguelarman@gmail.com>
2018-04-06 20:27:07 +0200

    aoiusd

--- Assignment3/src/junit/AppTest.java
+++ Assignment3/src/junit/AppTest.java
@@ -7,17 +7,8 @@ import org.junit.Test;
 import application.App;
 import application.dates.ModifiableDate;
 import application.offer.House;
-import application.users.Host;
-import application.users.RegisteredUser;
-import exceptions.AUserIsAlreadyLoggedException;
-import exceptions.IncorrectPasswordException;
-import exceptions.InvalidDateException;
-import exceptions.InvalidRolException;
-import exceptions.NoUserLoggedException;
-import exceptions.NotTheOwnerException;
-import exceptions.OfferAlreadyCreatedException;
-import exceptions.UnexistentUserException;
-import exceptions.UserIsBannedException;
+import application.users.*;
+import exceptions.*;
 
 public class AppTest {
 
@@ -64,9 +55,11 @@ public class AppTest {
 		}
 
 		try {
+			app.logout();
 			app.login("51999111X", "swordFish");
 			RegisteredUser userToBan = App.getLoggedUser();
-
+			
+			app.logout();
 			app = App.openApp();
 			app.login("X1130055", "secret");
 			app.banUser(userToBan);
@@ -92,6 +85,7 @@ public class AppTest {
 		}
 
 		app = App.openApp();
+		app.logout();
 
 		try {
 			app.login("nouserwiththis", "swordFish");
@@ -158,9 +152,11 @@ public class AppTest {
 			e.printStackTrace();
 			fail();
 		}
-		app.logout();
-		
 		int before = app.getOffers().size();
+		app.logout();
+		app.closeApp();
+		
+		
 		
 		ModifiableDate.plusDays(100);
 		
@@ -169,6 +165,81 @@ public class AppTest {
 	}
 	
 	// deleteExpiredReservations
+
+	@Test
+	public void testDeleteExpiredReservations() {
+		App app = App.openApp();
+		app.logout();
+		
+		try {
+			app.login("51999111X", "swordFish");
+		} catch (UserIsBannedException | IncorrectPasswordException | UnexistentUserException
+				| AUserIsAlreadyLoggedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+		
+		Host host = (Host)App.getLoggedUser();
+		
+		House house = new House(28049, "Cantoblanco", host);
+		
+		try {
+			app.createHolidayOffer(ModifiableDate.getModifiableDate().plusDays(3), 0.0, 0.0, "description", house, ModifiableDate.getModifiableDate().plusDays(18));
+		} catch (InvalidRolException | NoUserLoggedException | InvalidDateException | NotTheOwnerException
+				| OfferAlreadyCreatedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+		
+		app.logout();
+		app.closeApp();
+		
+		app = App.openApp();
+		
+		try {
+			app.login("55555111Z", "ItIsNoTKnOwN");
+		} catch (UserIsBannedException | IncorrectPasswordException | UnexistentUserException
+				| AUserIsAlreadyLoggedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			fail();
+		}
+		
+		try {
+			app.addReservation(app.getOffers().get(0));
+		} catch (InvalidRolException | RestrictedUserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+		
+		int before = 0;
+		
+		for (RegisteredUser u : app.getAuthorizedUsers()) {
+			before += u.getReservations().size();
+		}
+		
+		app.logout();
+		app.closeApp();
+		
+		ModifiableDate.plusDays(100);
+		
+		app = App.openApp();
+		
+		int after = 0;
+
+		for (RegisteredUser u : app.getAuthorizedUsers()) {
+			after += u.getReservations().size();
+		}
+		
+		assertEquals(after, before - 1, 0);
+	}
+	
+	
+	
+	
 	// deleteExpiredPendingOffers (suggestChanges) (approveOffer) (requestRevision)
 	
 	
