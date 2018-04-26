006b0d75f8dc8432eaf76b60311f857334256baa
albertogk <agk1esoc@gmail.com>
2018-04-26 19:19:35 +0200

    asd

--- Assignment4/src/windows/SearchWindow.java
+++ Assignment4/src/windows/SearchWindow.java
@@ -1,11 +1,15 @@
 package windows;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 import application.users.RegisteredUser;
 import controllers.SearchController;
@@ -16,17 +20,31 @@ public class SearchWindow extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 5035536669428890618L;
-	JRadioButton dates;
-	JRadioButton zipCode;
-	JRadioButton offerType;
-	JRadioButton booked;
-	JRadioButton paid;
-	JRadioButton avgRating;
+	private JRadioButton dates;
+	private JRadioButton zipCode;
+	private JRadioButton offerType;
+	private JRadioButton booked;
+	private JRadioButton paid;
+	private JRadioButton avgRating;
+	
+	final private JTextField zipCodeField;
+	final private JComboBox<String> offerTypeBox;
+	
 	
 	public SearchWindow(RegisteredUser user) {
 		super("Search offers");
 		
-		JPanel options = new JPanel(new FlowLayout());
+		JPanel options;
+		Container cont = super.getContentPane();
+		JPanel buttons = new JPanel(new GridLayout(1, 2));
+		if(user == null) {
+			options = new JPanel(new GridLayout(3, 2));
+		}
+		else {
+			options = new JPanel(new GridLayout(6, 2));
+		}
+		String[] typesOfOffer = {"Living offer", "Holiday offer"};
+		offerTypeBox = new JComboBox<String>(typesOfOffer);
 		dates = new JRadioButton("Search by dates");
 		zipCode = new JRadioButton("Search by ZIP code");
 		offerType = new JRadioButton("Search by type of offer");
@@ -37,9 +55,12 @@ public class SearchWindow extends JFrame {
 		group.add(zipCode);
 		group.add(offerType);
 		
-		options.add(dates);
+		zipCodeField = new JTextField();
 		options.add(zipCode);
+		options.add(zipCodeField);
 		options.add(offerType);
+		options.add(offerTypeBox);
+		options.add(dates);
 		
 		if(user != null) {
 			booked = new JRadioButton("Search booked offers");
@@ -54,7 +75,9 @@ public class SearchWindow extends JFrame {
 			options.add(paid);
 			options.add(avgRating);
 		}
-		this.setContentPane(options);
+		cont.add(options, BorderLayout.CENTER);
+		cont.add(buttons, BorderLayout.SOUTH);
+		this.setContentPane(cont);
 		this.setSize(400, 500);
 		this.setVisible(false);
 		
